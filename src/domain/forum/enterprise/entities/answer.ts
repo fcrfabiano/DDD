f3acb514d23a8aaa8a48ec44732f1fb13fb9e300
Fabiano César <fcfabiano.cesar@gmail.com>
2025-10-16 18:06:00 -0300

    feat: create a publisher for answer created

--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -1,9 +1,10 @@
 // -- IMPORTS
 
-import { Entity } from '@core/entities/entity';
 import { UniqueEntityId } from '@core/entities/unique_entity_id';
 import { Optional } from '@core/types/optional';
 import { AnswerAttachmentList } from './answer_attachment_list';
+import { AggregateRoot } from '@core/entities/aggregate_root';
+import { AnswerCreatedEvent } from '../events/answer_created_event';
 
 // -- TYPES
 
@@ -17,7 +18,7 @@ export interface AnswerProps
     updatedAt?: Date;
 }
 
-export class Answer extends Entity<AnswerProps>
+export class Answer extends AggregateRoot<AnswerProps>
 {
     // -- INQUIRIES
 
@@ -93,6 +94,13 @@ export class Answer extends Entity<AnswerProps>
             },
             id
             );
+
+        const isNewAnswer = !id;
+
+        if ( isNewAnswer )
+        {
+            answer.addDomainEvent( new AnswerCreatedEvent( answer ) );
+        }
 
         return answer;
     }
---
+++ src/domain/forum/enterprise/events/answer_created_event.ts
@@ -0,0 +1,33 @@
+// -- IMPORTS
+
+import { DomainEvent } from '@core/events/domain_event';
+import { Answer } from '../entities/answer';
+import { UniqueEntityId } from '@core/entities/unique_entity_id';
+
+// -- TYPES
+
+export class AnswerCreatedEvent implements DomainEvent
+{
+    // -- ATTRIBUTES
+
+    public ocurredAt: Date;
+    private answer: Answer; 
+
+    // -- CONSTRUCTOR
+
+    constructor(
+        answer: Answer
+        )
+    {
+        this.answer = answer;
+        this.ocurredAt = new Date();
+    }
+
+    // -- INQUIRIES
+
+    public getAggregateId(
+        )
+    {
+        return this.answer.id;        
+    }
+}
