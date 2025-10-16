dc3ca865d491345cd83c3ffe1fb23543e16367a7
Fabiano César <fcfabiano.cesar@gmail.com>
2025-10-16 19:50:59 -0300

    fix: adjusted set best answer id logic

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -154,7 +154,12 @@ export class Question extends AggregateRoot<QuestionProps>
         bestAnswerId: UniqueEntityId | undefined
         )
     {
-        if ( bestAnswerId && bestAnswerId !== this.props.bestAnswerId )
+        if ( bestAnswerId === undefined )
+        {
+            return;
+        }
+    
+        if ( this.props.bestAnswerId === undefined || !this.props.bestAnswerId.equals( bestAnswerId ) )
         {
             this.addDomainEvent( new QuestionBestAnswerChosenEvent( this, bestAnswerId ) );
         }
